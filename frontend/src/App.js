16ff2b9f6e3c6d2f78e893d8f2061d49cdea87f7
Dragonprod <artyom2002-98@mail.ru>
2022-04-12 23:53:54 +0300

    logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import LoginPage from './pages/login/login_page';
+import LoginPage from './pages/login/loginPage';
+import logo from './assets/logo_sumirea.png';
 
 function App() {
+  const [isLoading, setisLoading] = useState(true)
+
+  useEffect(() => {
+    let timer = setTimeout(() => setisLoading(false), 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <LoginPage />
+    <div className="container">
+      {isLoading ? <img src={logo}></img> : <LoginPage />}
     </div>
   );
 }
